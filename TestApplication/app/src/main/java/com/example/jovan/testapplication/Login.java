27c50edb299c36894bb57edbccb82fef5738c77f
Jovan Marjanović <Jovan Marjanovi>
2017-02-15 11:01:48 +0100

    Added isNetworkAvailable() for internet check

--- TestApplication/app/src/main/java/com/example/jovan/testapplication/Login.java
+++ TestApplication/app/src/main/java/com/example/jovan/testapplication/Login.java
@@ -1,14 +1,38 @@
 package com.example.jovan.testapplication;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    EditText mailText;
+    EditText passText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Login");
+
+        mailText = (EditText) findViewById(R.id.editText);
+        passText = (EditText) findViewById(R.id.editText2);
     }
+    public void logIn(View view) {
+        if(isNetworkAvailable()){
+
+        }else Toast.makeText(getApplicationContext(), "No internet connection! Please turn on your internet.", Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
 }
