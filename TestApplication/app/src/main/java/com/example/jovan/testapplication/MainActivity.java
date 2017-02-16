5ca4d0d4ea81659238178d6d9d57c70eaa63b139
Jovan Marjanović <Jovan Marjanovi>
2017-02-16 18:07:37 +0100

    Finished settingBlogsList method

--- TestApplication/app/src/main/java/com/example/jovan/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/jovan/testapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -17,6 +18,7 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +26,11 @@ import static com.example.jovan.testapplication.Login.EXTRA_TOKEN;
 
 public class MainActivity extends AppCompatActivity {
     String tokenParameter;
+
+    Context context;
+
+    ArrayList<Blogs> blogsArrayList;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +42,40 @@ public class MainActivity extends AppCompatActivity {
         tokenParameter = extras.getString(EXTRA_TOKEN);
         Log.i("Token",tokenParameter);
 
+        blogsArrayList = new ArrayList<>();
+        context = this;
+        lv = (ListView)findViewById(R.id.listView);
+
 
         if (isNetworkAvailable()){
-
-
+            settingBlogsList();
         }else Toast.makeText(getApplicationContext(), "No internet connection! Please turn on your internet.", Toast.LENGTH_LONG).show();
 
     }
 
     private void settingBlogsList(){
-        JsonArrayRequest blogsListRequest = new JsonArrayRequest(Request.Method.GET, "http://blogsdemo.creitiveapps.com:16427/blogs?token="+tokenParameter, null, new Response.Listener<JSONArray>() {
+        JsonArrayRequest logInRequest = new JsonArrayRequest(Request.Method.GET, "http://blogsdemo.creitiveapps.com:16427/blogs?token="+tokenParameter, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 Log.i("Response", response.toString());
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        blogsArrayList.add(new Blogs(
+                                jsonObject.getInt("id"),
+                                jsonObject.getString("title"),
+                                jsonObject.getString("image_url"),
+                                jsonObject.getString("description")));
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    CustomListAdapter adapter = new CustomListAdapter(
+                            getApplicationContext(), R.layout.blogs_list, blogsArrayList
+                    );
+                    lv.setAdapter(adapter);
+
+                }
 
             }
 
@@ -72,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        MySingleton.getInstance(this).addToRequestQueue(blogsListRequest);
+        MySingleton.getInstance(this).addToRequestQueue(logInRequest);
 
     }
 
